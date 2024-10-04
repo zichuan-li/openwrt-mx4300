57b7f2157d181ba49561ef7d5295c0ee5a657edf
arix00 <15333224+arix00@users.noreply.github.com>
2024-10-04 09:45:52 -0700

    Update release.sh
    format release note
    
    Signed-off-by: arix00 <15333224+arix00@users.noreply.github.com>

--- release.sh
+++ release.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
+MD="../note.md"
 mkdir release
-
-cp bin/targets/qualcommax/ipq807x/openwrt-qualcommax-ipq807x-linksys_mx4300-* release/
-cp bin/targets/qualcommax/ipq807x/openwrt-qualcommax-ipq807x-linksys_mx4300.manifest release/
+cp  bin/targets/qualcommax/ipq807x/openwrt-qualcommax-ipq807x-linksys_mx4300-* release/
+cp  bin/targets/qualcommax/ipq807x/openwrt-qualcommax-ipq807x-linksys_mx4300.manifest release/
 cp bin/targets/qualcommax/ipq807x/openwrt-imagebuilder* release/
-
 cd release
+kernel=$(cat openwrt-qualcommax-ipq807x-linksys_mx4300.manifest | grep ^kernel)
+checksum=$(sha256sum *)
+echo "- $kernel
 
-cat openwrt-qualcommax-ipq807x-linksys_mx4300.manifest | grep ^kernel > ../note.md
-echo "
-**SHA256SUM**" >> ../note.md
-sha256sum * >> ../note.md
-echo '
+- sha256sum
+\`\`\`
+${checksum}
+\`\`\`
 
--------
-
-"kernel dependency error" [fix](https://github.com/arix00/openwrt-mx4300/blob/doc/snapshot-dependency-guide.md)' >> ../note.md
+- \"kernel dependency error\" [fix](https://github.com/arix00/openwrt-mx4300/blob/doc/snapshot-dependency-guide.md)" > $MD
 
 cd ..
-
